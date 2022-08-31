d21f340ef3d2fadec0b2573f025b745b392bc0c9
ss11mik <38486852+ss11mik@users.noreply.github.com>
2022-08-31 13:22:38 +0000

    Add files via upload

---
+++ common/cwk-css.css
@@ -0,0 +1,351 @@
+
+.page h1, .page h2, .page h3, .page h4, .page h5, .page h6 {
+color: black;
+margin: 0 0 0.6em;
+padding-bottom: 0.17em;
+padding-top: 0.5em;
+clear: both;
+}
+.page h1 {
+font-size: 180%;
+border-bottom: 1px solid #AAAAAA;
+font-weight: normal;
+}
+.page h2 {
+font-size: 150%;
+border-bottom: 1px solid #AAAAAA;
+font-weight: normal;
+}
+.page h3 {
+font-size: 132%;
+}
+.page h4 {
+font-size: 116%;
+}
+.page h5 {
+font-size: 100%;
+}
+.page h6 {
+font-size: 80%;
+}
+.page h3,.page h4,.page h5,.page h6 {
+font-weight: bold;
+}
+.page p,.toolbar p {
+line-height: 1.5em; 
+margin: 0.4em 0 0.5em;
+}
+div.clearerr {
+clear: both;
+}
+a.anchor:hover {
+text-decoration: none;
+background-color: white;
+color: black;
+}
+a.media.image:hover {
+text-decoration: none;
+background-color: white;
+}
+a.internal:link,a.internal:visited,a.media:link,a.media:visited {
+color: #0645ad;
+text-decoration: none;
+}
+a.internalnone:link,a.internalnone:visited {
+color: red;
+text-decoration: none;
+}
+a.internal:hover,a.media:hover {
+color: white;
+background-color: #3f4f5f;
+}
+div.page a.external {
+background: url("/common/img/wikilink.png") no-repeat right center ;
+padding-right: 13px;
+text-decoration: none;
+color: #0645ad;
+}
+div.page a.external:hover {
+color: white;
+background-color: #3f4f5f;
+}
+.page dt {
+font-weight: bold;
+}
+.page dd {
+line-height: 1.5em;
+}
+.page ol,.toolbar ol {
+margin: 0.3em 0 0 2em;
+padding: 0;
+line-height: 1.5em;
+list-style-type: decimal;
+list-style-image: none;
+}
+.page ul,.toolbar ul {
+margin: 0.3em 0 0 1.5em;
+line-height: 1.5em;
+padding: 0;
+list-style-type: square;
+list-style-image: url("/common/img/wikili.png");
+}
+.page li,.toolbar li {
+margin-bottom: 0.1em;
+}
+form {
+display: inline;
+}
+div.syntax, .page pre {
+overflow-x: auto;
+-ms-overflow-style: -ms-autohiding-scrollbar;
+background-color: #F9F9F9;
+border: 1px dashed #2F6FAB;
+line-height: 1.1em;
+padding: 1em;
+margin: 0 20px 0 0;
+}
+.syntax pre {
+position: relative;
+border: 0;
+}
+.syntax ol {
+line-height: 1.1em;
+margin: 0 0 0 3.2em;
+padding: 0;
+}
+code,pre,tt {
+/* font-size: 120%;  */
+}
+ul.bar_menu {
+list-style: none;
+padding: 0;
+margin: 0;
+}
+ul.bar_menu li {
+background-color: lightblue;
+border-style: solid solid none solid;
+border-width: 1px 1px 0 1px;
+border-color: #aaaaaa;
+display: inline;
+margin: 0 0.3em 0;
+overflow: visible;
+padding-bottom: 0.2em ;
+}
+ul.bar_menu li a {
+text-decoration: none !important;
+padding: 0 0.8em 0.2em ;
+}
+.selected {
+font-weight: bold;
+padding: 0 0.7em 0.3em !important;
+z-index: 3;
+text-decoration: none;
+background-color: lightblue;
+}    
+div.page {
+background: white;
+border: solid 1px #aaaaaa;
+padding: 0.5em;
+margin-right: 1em;
+max-width: 1024px;
+}
+div.bar,div.bar-left,div.bar-right {
+display: inline-block;
+margin: 0;
+padding: 0;
+}
+table.toc {
+table-layout: auto;
+}
+table.toc td,table.toc th {
+padding: 2px;
+background-color: #f9f9f9;
+vertical-align: top;
+}
+div.toc {
+background-color: #f9f9f9;
+border: solid 1px #aaaaaa;
+padding: 0.5em 1em 0.5em 0.5em;
+display: inline-block;
+float: right;
+margin: 0.5em;
+}
+div.toc_title {
+font-weight: bold;
+}
+.editsection {
+float: right;
+font-size: 67%;
+font-weight: normal;
+}
+.editsection a {
+text-decoration: none;
+}
+.leftpane {
+width: 200px;
+padding: 0.5em;
+}
+.rightpane {
+}
+.tb_main {
+background: white;
+border: 1px solid #aaaaaa;
+margin: 0 0.5em 1em 0;
+padding: 0.5em;
+}
+table.wikitable {
+background: none repeat scroll 0 0 #F9F9F9;
+border-collapse: collapse;
+border: 1px solid #AAAAAA;
+color: black;
+margin: 1em 1em 1em 0;
+}
+.wikitable th {
+background: #8da7d6;
+text-align: center;
+padding: 0.2em;
+border: 1px solid #AAAAAA;
+}
+.wikitable td {
+padding: 0.2em;
+border: 1px solid #AAAAAA;
+}
+table.inline {
+border-color: #cccccc;
+margin: 0.6em 0 0.6em 0;
+}
+.inline td, .inline th {
+padding: 0.2em;
+border-color: #cccccc;
+}
+.msglevel1 {
+color: red;
+font-weight: bold;
+}
+div.thumb {
+width: auto;
+clear: both;
+margin: 0.5em 1.4em 1.3em 0;
+border: 1px solid #cccccc;
+padding: 3px;
+}
+div.thumbright {
+width: auto;
+clear: right;
+float: right;
+margin: 0.5em 0 1.3em 1.4em;
+border: 1px solid #cccccc;
+padding: 3px;
+}
+div.thumbleft {
+width: auto;
+clear: left;
+float: left;
+margin: 0.5em 1.4em 1.3em 0;
+border: 1px solid #cccccc;
+padding: 3px;
+}
+div.thumbcenter {
+width: auto;
+clear: both;
+margin: 0.5em 1.4em 1.3em 1.4em;
+border: 1px solid #cccccc;
+padding: 3px;
+}
+div.thumbcaption {
+font-size: 94%;
+line-height: 1.4em;
+padding: 3px;
+}
+div.wiki_top {
+float: right;
+margin-right: 1em;
+}
+div.footer,div.clearer {
+clear: both;
+}
+ins.diffchange {
+color: green;
+font-weight: bold;
+text-decoration: none;
+word-wrap: break-word;
+}
+del.diffchange {
+color: red;
+font-weight: bold;
+text-decoration: none;
+}
+td.diff-marker {
+text-align: right;
+width: 1em;
+}
+td.diff-addedline,td.diff-context {
+width: 50%;
+word-wrap: break-word;
+}
+
+td.diff-deletedline {
+width: 50%;
+word-wrap: break-word;
+}
+abbr.newpage,abbr.minor {
+font-weight: bold;
+cursor: help;
+}
+span.history-comment {
+font-style: italic;
+}
+div.revision_info {
+color: #7f7f7f;
+font-size: 84%;
+}
+div.search-data {
+color: green;
+}
+div.warning {
+color: red;
+}
+div.center {
+text-align: center;
+}
+div.block-note {
+background-image: url("/images/bulbgraph.png");
+background-position: left top;
+background-repeat: no-repeat;
+}
+div.pagenav-prev {
+float: left;
+}
+div.pagenav-next {
+float: right;
+}
+span.error {
+color: red;
+}
+.block-contents {
+display: block;
+padding-left: 20px;
+}
+.tex {
+vertical-align: middle;
+}
+img.bw1 { 
+border: 1px solid #cccccc;!important
+}
+.home {
+color: #0063a5;
+font-weight: bold;
+}
+.page hr {
+border: 0 none;
+border-top: 1px solid #aaaaaa;
+height: 1px;
+}
+.page textarea {
+padding: 2px;
+border: gray 1px solid;
+background-color: #fff;
+}
+.page img {
+max-width: 100%;
+}
